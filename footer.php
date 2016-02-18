15a78f87a4c2c100e9016534acb2f715dc47b959
Paul Maitland <mantroon@Pauls-iMac.local>
2016-02-18 09:20:48 -0500

    Updated location

--- footer.php
+++ footer.php
@@ -1,14 +1,18 @@
-        <footer>
-            <small>Copyright &copy; <strong><?php bloginfo('name');?></strong> <?php the_date('Y');?></small>
-            <small><a href="http://www.bay12design.com/" title="Bay12 Design, Website Design in Columbus GA" target="_blank">Website Design by Bay12 Design</a></small>
-        </footer>
-        <?php wp_footer();?>
-        <!-- analytics -->
-		<script>
-			var _gaq=[['_setAccount','UA-XXXXXXXX-XX'],['_trackPageview']];
-			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-			s.parentNode.insertBefore(g,s)})(document,'script');
-		</script>
+<footer>
+    <small>Copyright &copy; <strong><?php bloginfo('name');?></strong> <?php the_date('Y');?></small>
+    <small><a href="http://www.bay12design.com/" title="Bay12 Design, Website Design in Acowrth GA" target="_blank">Website Design by Bay12 Design</a></small>
+</footer>
+<?php wp_footer();?>
+    <!-- analytics -->
+    <script>
+        var _gaq = [['_setAccount', 'UA-XXXXXXXX-XX'], ['_trackPageview']];
+        (function (d, t) {
+            var g = d.createElement(t),
+                s = d.getElementsByTagName(t)[0];
+            g.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g, s)
+        })(document, 'script');
+    </script>
     </body>
-</html>
+
+    </html>
