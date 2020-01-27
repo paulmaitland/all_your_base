9b856338d321399e635df0dd20a4e409c2667c71
Paul Maitland <paulmaitland@users.noreply.github.com>
2020-01-27 12:46:30 -0500

    Udted functions, added new boostrap code

--- functions.php
+++ functions.php
@@ -92,13 +92,13 @@ function load_scripts()
     if (!is_admin()) {
 
     wp_deregister_script('jquery');
-    wp_register_script('jquery', 'code.jquery.com/jquery-3.1.1.slim.min.js', array(), '3.1.1', true); 
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true); 
     wp_enqueue_script('jquery'); 
 
-    wp_register_script('tether', 'cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', array('jquery'), '1.4.0',true); 
-    wp_enqueue_script('tether'); 
+    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array('jquery'), '1.4.0',true); 
+    wp_enqueue_script('popper'); 
 	    
-	wp_register_script('bootstrap', 'maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js', array('jquery','tether'), '4.0.0',true); 
+	wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array('jquery','popper'), '4.0.0',true); 
     wp_enqueue_script('bootstrap'); 
     
     wp_register_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery','bootstrap'), '1.0.0',true); 
@@ -128,7 +128,7 @@ add_action('wp_print_scripts', 'conditional_scripts');
 
 function theme_styles()
 {
-	wp_register_style('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css', array(), '4.0.0', 'all');
+	wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all');
     wp_enqueue_style('bootstrap'); 
 	wp_register_style('fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array('bootstrap'), '4.7.0', 'all');
     wp_enqueue_style('fontawesome');
